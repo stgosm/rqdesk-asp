747c87ed3050943c2a0f267d0918310872aeba82
Alexis <Alexis@TRABAJO2>
2020-11-06 00:54:14 -0600

    Se agrego Validacion de usuarios con la base de datos, Se agrego vista de registro de usuarios para administrador y modelo de base de datos SQL

---
+++ rqdesk/Registro_Usuarios.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace rqdesk
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+     
+
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+          string  usuarioregistro = txtUsuarioRegistro.Text;
+            string contrasenaregistro = txtContraseñaRegistro.Text;
+            string privilegioregistro = SelectPriv.Value;
+
+
+        }
+    }
+}
--- rqdesk/home.aspx
+++ rqdesk/home.aspx
@@ -2,5 +2,5 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>Alexis es pu**</h1>
+    <h1>Santi me la pela </h1>
 </asp:Content>
