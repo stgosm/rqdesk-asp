c195a7c6713cf8b2b5856e3c161c1c091dde4b95
Santiago Serrano Márquez <santiago.serrano.marquez@hotmail.com>
2020-11-11 20:15:35 -0600

    Requirements model

--- rqdesk/requirements.aspx.cs
+++ rqdesk/requirements.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace rqdesk
 {
@@ -11,7 +12,25 @@ namespace rqdesk
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            using (SqlConnection cn = new SqlConnection("Data Source=TRABAJO2;Initial Catalog=RQ_final;Integrated Security=True "))
+            {
+                SqlCommand cmd = new SqlCommand("select usuario, contrasena FROM usuario WHERE usuario ='" + us + "' AND contrasena = '" + pw + "'", cn);
+                cn.Open();
 
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    Session["id"] = txtUsuario.Text;
+                    Response.Redirect("escritorio.aspx");
+                    cn.Close();
+                }
+
+                else
+                {
+                    Response.Write("<script type='text/javascript'>alert('Datos no Encontrados')</script>");
+                }
+            }
         }
     }
 }
